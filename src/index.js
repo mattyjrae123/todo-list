2d1f9bab0089771c215fc5406e6ef27758bc6bc6
Matthew Rae <mattyjrae123@hotmail.com>
2023-12-10 21:30:16 +0700

    Testing modal button functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './styles/style.css';
 
 console.log("noodle is here");
+
+// const modalBackground = document.querySelector("#modal-bg");
+// const listModal = document.querySelector("#list-add-modal");
+// const itemModal = document.querySelector("#item-add-modal");
+
+// /**
+//  * List Add Modal
+//  */
+// document.querySelector("#list-add-btn")
+//   .addEventListener("click", () => {
+//     modalBackground.classList.remove("hidden");
+//     listModal.classList.remove("hidden");
+
+//   });
+
+//   /**
+//    * Item Add Modal
+//    */
+// document.querySelector("#item-add-btn")
+//   .addEventListener("click", () => {
+//     modalBackground.classList.remove("hidden");
+//     itemModal.classList.remove("hidden");
+
+//   });
