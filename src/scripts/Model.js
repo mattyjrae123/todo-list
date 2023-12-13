4f7dc0118f4c70fa620d284cb3329afcccba8f03
Matthew Rae <mattyjrae123@hotmail.com>
2023-12-13 14:52:08 +0700

    Add underscores to private properties of Model

--- src/scripts/Model.js
+++ src/scripts/Model.js
@@ -6,31 +6,31 @@
  * M Rae 11/12/2023
  */
 const Model = (() => {
-  const projects = [];
+  const _projects = [];
 
   const addProject = (title) => {
-    if (!stringIsValid(title)) {
+    if (!_stringIsValid(title)) {
       console.error('addProject string parameter invalid');
       return;
     }
 
-    const id = generateProjectId();
+    const id = _generateProjectId();
 
-    projects.push({
+    _projects.push({
       id,
       title,
       todos: []
     });
   }
 
-  const generateProjectId = () => {
-    if (projects.length <= 0) {
+  const _generateProjectId = () => {
+    if (_projects.length <= 0) {
       return 1;
     }
-    return projects[projects.length - 1].id + 1;
+    return _projects[_projects.length - 1].id + 1;
   }
 
-  const stringIsValid = (input) => {
+  const _stringIsValid = (input) => {
     if (!(typeof input === 'string' || input instanceof String)) {
       return false;
     }
@@ -43,38 +43,38 @@ const Model = (() => {
   }
 
   const getProjects = () => {
-    return [...projects];
+    return [..._projects];
   }
 
-  const getProjectIndex = (id) => {
+  const _getProjectIndex = (id) => {
     if (!(typeof id === 'number' || id instanceof Number)) {
       return -1;
     }
 
-    for (let i = 0; i < projects.length; i++) {
-      if (projects[i].id === id) {
+    for (let i = 0; i < _projects.length; i++) {
+      if (_projects[i].id === id) {
         return i;
       }
     }
-    
+
     return -1;
   }
 
   const addItem = (projectId, priority, title, description, date) => {
-    const projectIndex = getProjectIndex(projectId);
-    
+    const projectIndex = _getProjectIndex(projectId);
+
     if (projectIndex <= -1) {
       console.error("Project ID invalid");
       return false;
     }
 
-    if (!(stringIsValid(title) || stringIsValid(description) || stringIsValid(date))) {
+    if (!(_stringIsValid(title) || _stringIsValid(description) || _stringIsValid(date))) {
       console.error("New todo item parameters invalid");
       return false;
     }
 
-    const id = generateTodoId(projectIndex);
-    
+    const id = _generateTodoId(projectIndex);
+
     const todo = {
       id,
       priority,
@@ -84,19 +84,19 @@ const Model = (() => {
       complete: false
     };
 
-    projects[projectIndex].todos.push(todo);
-    generateTodoId(projectIndex);
+    _projects[projectIndex].todos.push(todo);
+    _generateTodoId(projectIndex);
     return true;
   }
 
-  const generateTodoId = (projectIndex) => {
-    const todos = projects[projectIndex].todos;
+  const _generateTodoId = (projectIndex) => {
+    const todos = _projects[projectIndex].todos;
 
     if (todos.length <= 0) {
       return 1;
     }
 
-    return todos[todos.length-1].id + 1;
+    return todos[todos.length - 1].id + 1;
   }
 
   return {
