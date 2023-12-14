d5778dc1b86705541bc36508236e899fa2e67a6f
Matthew Rae <mattyjrae123@hotmail.com>
2023-12-14 15:21:43 +0700

    Add underscore to private View members

--- src/scripts/View.js
+++ src/scripts/View.js
@@ -7,42 +7,42 @@
  */
 const View = (() => {
 
-  const modalBg = document.querySelector("#modal-bg");
-  const listAddModalBtn = document.querySelector("#list-add-btn");
-  const listAddModal = document.querySelector("#list-add-modal");
-  const listCancelBtn = document.querySelector("#list-add-cancel");
+  const _modalBg = document.querySelector("#modal-bg");
+  const _listAddModalBtn = document.querySelector("#list-add-btn");
+  const _listAddModal = document.querySelector("#list-add-modal");
+  const _listCancelBtn = document.querySelector("#list-add-cancel");
 
-  const itemAddModalBtn = document.querySelector("#item-add-btn");
-  const itemAddModal = document.querySelector("#item-add-modal");
-  const itemCancelBtn = document.querySelector("#item-add-cancel");
+  const _itemAddModalBtn = document.querySelector("#item-add-btn");
+  const _itemAddModal = document.querySelector("#item-add-modal");
+  const _itemCancelBtn = document.querySelector("#item-add-cancel");
 
   const initEventListeners = (projectsExist) => {
-    listAddModalBtn.addEventListener("click", () => {
-      modalBg.classList.remove("hidden");
-      listAddModal.classList.remove("hidden");
+    _listAddModalBtn.addEventListener("click", () => {
+      _modalBg.classList.remove("hidden");
+      _listAddModal.classList.remove("hidden");
     });
 
-    listCancelBtn
+    _listCancelBtn
       .addEventListener("click", () => {
-        modalBg.classList.add("hidden");
+        _modalBg.classList.add("hidden");
 
-        listAddModal.classList.add("hidden");
+        _listAddModal.classList.add("hidden");
       })
 
-    itemAddModalBtn
+    _itemAddModalBtn
       .addEventListener("click", () => {
         if (!projectsExist()) {
           window.alert("You must create a project first!");
           return;
         }
         
-        modalBg.classList.remove("hidden");
-        itemAddModal.classList.remove("hidden");
+        _modalBg.classList.remove("hidden");
+        _itemAddModal.classList.remove("hidden");
       });
 
-    itemCancelBtn.addEventListener("click", () => {
-      modalBg.classList.add("hidden");
-      itemAddModal.classList.add("hidden");
+    _itemCancelBtn.addEventListener("click", () => {
+      _modalBg.classList.add("hidden");
+      _itemAddModal.classList.add("hidden");
     });
   }
 
