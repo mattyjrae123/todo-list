e652a6ad6edb7d9d16b3f8984e99ee1f8d0bed51
Matthew Rae <mattyjrae123@hotmail.com>
2023-12-14 14:34:59 +0700

    Add event listener to show item-item-add-modal

--- src/scripts/View.js
+++ src/scripts/View.js
@@ -8,12 +8,21 @@
 const View = (() => {
 
   const modalBg = document.querySelector("#modal-bg");
+  const listAddBtn = document.querySelector("#list-add-btn");
+  const itemAddBtn = document.querySelector("#item-add-btn");
   const listAddModal = document.querySelector("#list-add-modal");
+  const itemAddModal = document.querySelector("#item-add-modal");
 
-  document.querySelector("#list-add-btn")
+  listAddBtn
   .addEventListener("click", () => {
     modalBg.classList.remove("hidden");
     listAddModal.classList.remove("hidden");
+  });
+
+  itemAddBtn
+  .addEventListener("click", () => {
+    modalBg.classList.remove("hidden");
+    itemAddModal.classList.remove("hidden");
   });
 
 })();
